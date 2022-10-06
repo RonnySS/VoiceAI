07d5d3d729da1b496fdfc4e31faa966726be0966
Redbone <donghaojie@wasu.com>
2022-10-06 23:32:03 +0800

    module step OK

---
+++ lib_base/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = AppConfig.compileSdk
+    buildToolsVersion = AppConfig.buildToolsVersion
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+        version = AppConfig.versionCode
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "roguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(DependenciesConfig.KTX_CORE)
+    implementation(DependenciesConfig.APP_COMPAT)
+    implementation(DependenciesConfig.MATERIAL)
+}
---
+++ lib_voice/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = AppConfig.compileSdk
+    buildToolsVersion = AppConfig.buildToolsVersion
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+        version = AppConfig.versionCode
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "roguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+}
