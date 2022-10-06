cecee894c7fdd1bff39f689d41a9e089de324d3d
Redbone <donghaojie@wasu.com>
2022-10-06 14:59:27 +0800

    app gradle file OK!

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,15 +17,15 @@ android {
 
     }
 
-//    signingConfigs {
-//        register("release"){
-//            keyAlias = "imooc"
-//            keyPassword = "123456"
-//            storeFile = file("src/main/jks/Untitled")
-//            storePassword = "123456"
-//
-//        }
-//    }
+    signingConfigs {
+        register("release"){
+            keyAlias = "imooc"
+            keyPassword = "123456"
+            storeFile = file("src/main/jks/SignFile")
+            storePassword = "123456"
+
+        }
+    }
 
 
     //编译类型，混淆
@@ -35,19 +35,19 @@ android {
         }
         getByName("release") {
             isMinifyEnabled = false
-//            signingConfig = signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "roguard-rules.pro")
         }
     }
 
-//    android.applicationVariants.all{
-//        val buildType = this.buildType
-//        outputs.all {
-//            if(this is com.android.build.gradle.internal.api.ApkVariantOutputImpl){
-//                this.outputFileName = "AI_V${defaultConfig.versionName}_${buildType.name}.apk"
-//            }
-//        }
-//    }
+    android.applicationVariants.all{
+        val buildType = this.buildType
+        outputs.all {
+            if(this is com.android.build.gradle.internal.api.ApkVariantOutputImpl){
+                this.outputFileName = "AI_V${defaultConfig.versionName}_${buildType.name}.apk"
+            }
+        }
+    }
 
     //依赖操作
     compileOptions {
