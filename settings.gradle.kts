368a53e13e87dcedcdaabd49b5f4c8d6e2ce4602
Redbone <donghaojie@wasu.com>
2022-10-06 15:28:13 +0800

    now start module change

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,14 @@ dependencyResolutionManagement {
 rootProject.name = "VoiceAI"
 include(":app")
 rootProject.buildFileName = "build.gradle.kts"
+include(":lib_base")
+include(":lib_network")
+include(":lib_voice")
+include(":module_app_manager")
+include(":module_constellation")
+include(":module_developer")
+include(":module_joke")
+include(":module_map")
+include(":module_setting")
+include(":module_voice_setting")
+include(":module_weather")
